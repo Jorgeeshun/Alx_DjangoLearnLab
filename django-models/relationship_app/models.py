a74bbd9ee7772e27c53046b3635b8c31ee3da217
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2024-08-18 20:28:42 +0000

    Implementing Custom Permissions in Django

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -59,3 +59,20 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add a book'),
+            ('can_change_book', 'Can change a book'),
+            ('can_delete_book', 'Can delete a book'),
+        ]
+
+    def __str__(self):
+        return self.title
+    
