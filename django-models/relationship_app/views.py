62b1d207cef96e5f5a37393a07b9ce1f7b2e0f45
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2024-08-18 19:12:30 +0000

    i Changes to be committed: 	modified:   django-models/relationship_app/views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, 
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from .models import Book
 
