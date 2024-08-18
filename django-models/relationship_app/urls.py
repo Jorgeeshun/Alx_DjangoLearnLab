d75ae86ee22a148f27fe1d08dfb3f0ee67335dec
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2024-08-18 19:15:45 +0000

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   django-models/relationship_app/urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import login_view, logout_view, register_view
+from . import views
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('register/', register_view, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register_view, name='register'),
 ]
